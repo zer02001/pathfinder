c7044944d0b3ddfd6582f8a292672329119aadea
KLavallee <lavallee.kyle@gmail.com>
2020-12-05 12:41:55 -0500

    Finish header file

---
+++ dotFiles/pathFinder.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "position.h"
+#include "cell.h"
+using namespace std;
+
+class pathFinder {
+    private: 
+        cell *map;
+        position currentPosition;
+        void display();
+    public:
+        void search();
+        pathFinder(cell *pmap, int initialColumn, int initialRow);
+}
