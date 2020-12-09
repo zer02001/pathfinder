f1d36aa66377c9f0f45e36aa7ada2748d8ebbaff
zer02001 <60124033+zer02001@users.noreply.github.com>
2020-12-08 20:13:15 -0500

    Update main.cpp

--- dotFiles/main.cpp
+++ dotFiles/main.cpp
@@ -6,4 +6,41 @@ using namespace std;
 
 int main() {
     
+
+
+	cell cells[5][6];
+
+
+	for (int i = 0; i < 5; i++) {
+	
+		for (int f = 0; f < 6; f++) {
+			if (i == 0) {
+
+				cells[i][f].setCellType(0);
+
+			}
+			else if (i == 1 && f == 5) {
+
+				cells[i][f].setCellType(2)
+
+			}
+
+			else cells[i][f].setCellType(1);
+
+		}
+	
+	}
+
+	cell** Pmap = cells;
+
+	pathFinder path(Pmap, 6, 5);
+
+	path.search();
+	path.display();
+
+
+
+
+
+
 }
